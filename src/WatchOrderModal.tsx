8866fc79196d3d1ecdb5343d6c82ce018a9bd972
Adrian Nguyen <hngt.adrian@gmail.com>
2023-05-03 19:44:10 -0700

    Import proper Material UI components

--- src/WatchOrderModal.tsx
+++ src/WatchOrderModal.tsx
@@ -1,4 +1,10 @@
 import React, { useState, useEffect } from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import Button from "@mui/material/Button";
+import Box from '@mui/material/Box';
 
 interface WatchOrderModalProps {
 
@@ -7,7 +13,7 @@ interface WatchOrderModalProps {
 const WatchOrderModal = () => {
     return (
         <div>
-            
+
         </div>
     );
 };
