36afdf10efa14ad202e0eccf68405f53d0eef991
Adrian Nguyen <hngt.adrian@gmail.com>
2023-05-04 22:56:16 -0700

    Fix spacing of Dialog box

--- src/components/WatchOrderModal/WatchOrderModal.tsx
+++ src/components/WatchOrderModal/WatchOrderModal.tsx
@@ -29,7 +29,6 @@ const colors = {
 const WatchOrderModal = (props: WatchOrderModalProps) => {
   const { open, onClose } = props;
 
-
   const [orderData, setOrderData] = useState<any>(null);
 
   // Fetching data from API
@@ -52,7 +51,7 @@ const WatchOrderModal = (props: WatchOrderModalProps) => {
   if (!orderData) {
     return <div></div>;
   }
-  
+
   // Extracting data fetched into constants
   const {
     listing: {
@@ -69,21 +68,24 @@ const WatchOrderModal = (props: WatchOrderModalProps) => {
 
   // Converting cents to display format
   const centsToDollars = (cents: number) => {
-    return (cents / 100).toLocaleString("en-US", { style: "currency", currency: "USD" });
+    return (cents / 100).toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
   };
 
   return (
-  <Dialog
-    open={open}
-    onClose={onClose}
-    PaperProps={{
-      sx: {
-        borderRadius: "35px",
-        width: "830px",
-        maxWidth: "70vw",
-      },
-    }}
-  >
+    <Dialog
+      open={open}
+      onClose={onClose}
+      PaperProps={{
+        sx: {
+          borderRadius: "35px",
+          width: "830px",
+          maxWidth: "70vw",
+        },
+      }}
+    >
       <Box
         style={{
           backgroundColor: colors.modalBackgroundColor,
@@ -141,7 +143,7 @@ const WatchOrderModal = (props: WatchOrderModalProps) => {
                     style={{
                       backgroundColor: colors.primaryGreen,
                       borderRadius: "25px",
-                      height: "58px",
+                      height: "62px",
                       textTransform: "none",
                       fontWeight: "medium",
                     }}
@@ -156,7 +158,7 @@ const WatchOrderModal = (props: WatchOrderModalProps) => {
                     style={{
                       color: colors.primaryGreen,
                       borderRadius: "25px",
-                      height: "58px",
+                      height: "62px",
                       textTransform: "none",
                       fontWeight: "medium",
                     }}
@@ -170,124 +172,136 @@ const WatchOrderModal = (props: WatchOrderModalProps) => {
 
             <Grid item xs={12} md={6}>
               <Box padding={"10px"}>
-              <Box
-                style={{ backgroundColor: colors.billingBackgroundColor }}
-                padding={"30px"}
-                borderRadius={"35px"}
-              >
-                <Divider sx={{ mt: 1, mb: 1 }}/>
-                <Box display="flex" alignItems="center">
-                  <Box flexGrow={1}>
-                    <Typography variant="body2" color={colors.primaryGreen} sx={{ mb: 1 }}>
-                      {displayName}<br/>
-                      {brand.displayName} {referenceNumber}
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      color={colors.secondaryBillingText}
-                    >
-                      {condition} / {manufactureYear}
-                    </Typography>
-                  </Box>
-                  <Box>
-                    <img
-                      src={images[0].image.url}
-                      //alt={`${/* Watch display name */} ${/* Watch  name */}`}
-                      style={{ width: 75, height: 75, borderRadius: "12px" }}
-                    />
-                  </Box>
-                </Box>
-                <Divider sx={{ mt: 1, mb: 2 }}/>
-                <Box my={2}>
-                  <Grid container>
-                    <Grid item xs={6}>
-                      <Typography
-                        variant="body2"
-                        color={colors.secondaryBillingText}
-                        sx={{ mb: 1 }}
-                      >
-                        Selling Price
-                      </Typography>
-                      <Typography
-                        variant="body2"
-                        color={colors.secondaryBillingText}
-                        sx={{ mb: 1 }}
-                      >
-                        Level 1 Commission ({(commissionRateBips / 100).toFixed(1)}%)
-                      </Typography>
-                      <Typography
-                        variant="body2"
-                        color={colors.secondaryBillingText}
-                        sx={{ mb: 1 }}
-                      >
-                        Seller Fee
-                      </Typography>
-                      <Typography variant="body2" color={colors.secondaryGreen} sx={{ mb: 1 }}>
-                        Bezel authentification
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={6}>
-                      <Typography
-                        variant="body2"
-                        color={colors.primaryGreenText}
-                        align="right"
-                        fontWeight={"bold"}
-                        sx={{ mb: 1 }}
-                      >
-                        {centsToDollars(salePriceCents)}
-                      </Typography>
-                      <Typography
-                        variant="body2"
-                        color={colors.secondaryBillingText}
-                        align="right"
-                        sx={{ mb: 1 }}
-                      >
-                        {centsToDollars((salePriceCents * commissionRateBips) / 10000)}
-                      </Typography>
-                      <Typography
-                        variant="body2"
-                        color={colors.secondaryBillingText}
-                        align="right"
-                        sx={{ mb: 1 }}
-                      >
-                        {centsToDollars(sellerFeeCents)}
-                      </Typography>
-                      <Typography
-                        variant="body2"
-                        color={colors.secondaryGreen}
-                        align="right"
-                        sx={{ mb: 1 }}
-                      >
-                        Free
-                      </Typography>
-                    </Grid>
-                  </Grid>
-                </Box>
-                <Divider sx={{ mt: 1, mb: 1 }} />
-                <Box my={2}>
-                  <Grid container>
-                    <Grid item xs={6}>
+                <Box
+                  style={{ backgroundColor: colors.billingBackgroundColor }}
+                  padding={"30px"}
+                  borderRadius={"35px"}
+                >
+                  <Divider sx={{ mt: 1, mb: 1 }} />
+                  <Box display="flex" alignItems="center">
+                    <Box flexGrow={1}>
                       <Typography
                         variant="body2"
                         color={colors.primaryGreen}
-                        fontWeight={"bold"}
+                        sx={{ mb: 1 }}
                       >
-                        Earnings
+                        {displayName}
+                        <br />
+                        {brand.displayName} {referenceNumber}
                       </Typography>
-                    </Grid>
-                    <Grid item xs={6}>
                       <Typography
                         variant="body2"
-                        color={colors.primaryGreen}
-                        align="right"
-                        fontWeight={"bold"}
+                        color={colors.secondaryBillingText}
                       >
-                        {centsToDollars(payoutAmountCents)}
+                        {condition} / {manufactureYear}
                       </Typography>
+                    </Box>
+                    <Box>
+                      <img
+                        src={images[0].image.url}
+                        //alt={`${/* Watch display name */} ${/* Watch  name */}`}
+                        style={{ width: 75, height: 75, borderRadius: "12px" }}
+                      />
+                    </Box>
+                  </Box>
+                  <Divider sx={{ mt: 1, mb: 2 }} />
+                  <Box my={2}>
+                    <Grid container>
+                      <Grid item xs={7}>
+                        <Typography
+                          variant="body2"
+                          color={colors.secondaryBillingText}
+                          sx={{ mb: 1 }}
+                        >
+                          Selling Price
+                        </Typography>
+                        <Typography
+                          variant="body2"
+                          color={colors.secondaryBillingText}
+                          sx={{ mb: 1 }}
+                        >
+                          Level 1 Commission (
+                          {(commissionRateBips / 100).toFixed(1)}%)
+                        </Typography>
+                        <Typography
+                          variant="body2"
+                          color={colors.secondaryBillingText}
+                          sx={{ mb: 1 }}
+                        >
+                          Seller Fee
+                        </Typography>
+                        <Typography
+                          variant="body2"
+                          color={colors.secondaryGreen}
+                          sx={{ mb: 2 }}
+                        >
+                          Bezel authentification
+                        </Typography>
+                      </Grid>
+                      <Grid item xs={5}>
+                        <Typography
+                          variant="body2"
+                          color={colors.primaryGreenText}
+                          align="right"
+                          fontWeight={"bold"}
+                          sx={{ mb: 1 }}
+                        >
+                          {centsToDollars(salePriceCents)}
+                        </Typography>
+                        <Typography
+                          variant="body2"
+                          color={colors.secondaryBillingText}
+                          align="right"
+                          sx={{ mb: 1 }}
+                        >
+                          {centsToDollars(
+                            (salePriceCents * commissionRateBips) / 10000
+                          )}
+                        </Typography>
+                        <Typography
+                          variant="body2"
+                          color={colors.secondaryBillingText}
+                          align="right"
+                          sx={{ mb: 1 }}
+                        >
+                          {centsToDollars(sellerFeeCents)}
+                        </Typography>
+                        <Typography
+                          variant="body2"
+                          color={colors.secondaryGreen}
+                          align="right"
+                          sx={{ mb: 1 }}
+                        >
+                          Free
+                        </Typography>
+                      </Grid>
                     </Grid>
-                  </Grid>
+                  </Box>
+                  <Divider sx={{ mt: 1, mb: 1 }} />
+                  <Box my={2}>
+                    <Grid container>
+                      <Grid item xs={6}>
+                        <Typography
+                          variant="body2"
+                          color={colors.primaryGreen}
+                          fontWeight={"bold"}
+                        >
+                          Earnings
+                        </Typography>
+                      </Grid>
+                      <Grid item xs={6}>
+                        <Typography
+                          variant="body2"
+                          color={colors.primaryGreen}
+                          align="right"
+                          fontWeight={"bold"}
+                        >
+                          {centsToDollars(payoutAmountCents)}
+                        </Typography>
+                      </Grid>
+                    </Grid>
+                  </Box>
                 </Box>
-              </Box>
               </Box>
             </Grid>
           </Grid>
